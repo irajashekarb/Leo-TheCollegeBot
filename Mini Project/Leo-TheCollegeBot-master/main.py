891884f7a6766006ac94478a7c481610acec49f3
Raja Shekar <raja21shekar04@gmail.com>
2018-08-12 12:52:43 +0530

    Fifth Commit

--- Mini Project/Leo-TheCollegeBot-master/main.py
+++ Mini Project/Leo-TheCollegeBot-master/main.py
@@ -3,11 +3,43 @@ import tkinter as tk
 
 chat_window = Tk()
 chat_window.title("Hitam Bot")
+
+#Determining the size of the chat window
+#chat_window.geometry("450x600")
+chat_window.geometry('{}x{}'.format(450, 600))
+
+#Removing min/max button from the window to maintain the window
+chat_window.resizable(0,0)
+
+#Creating the top frame
+top = Frame(chat_window)
+top.pack(side=TOP,fill=X)
+
+#Creating the bottom frame
+bottom = Frame(chat_window)
+bottom.pack(side=BOTTOM,fill=X)
+
+#This displays the heading text on the top of the chat  window
+head_text = Label(top,text="Hi,I am Leo the hitam bot.I am new and still learning.Start chatting.")
+head_text.pack(side=TOP,fill=X)
+
+#Textfield in the bottom frame to take the user input
+input_user = StringVar()
+input_field = Entry(bottom,text=input_user)
+input_field.pack(fill=X,ipadx=10)
+
+#Send button in the bottom frame
+send_button = Button(bottom,text="Send")
+send_button.pack(fill=X,ipadx=10)
+
+
+
+#Main loop that runs the chat window over and over till the user terminates the process
 chat_window.mainloop()
 
 
 
-
+"""
 #importing the listtrainer
 from chatterbot.trainers import ListTrainer
 
@@ -48,7 +80,7 @@ for _file in os.listdir('Conversations'):
 
 print('''************************HITAM BOT***********************************''')
 
-print("""Hi,I'm Leo the hitam bot.I'm new and still learning.Start chatting.""")
+print("Hi,I am Leo the hitam bot.I am new and still learning.Start chatting.")
 
 #Chat loop
 while True:
@@ -69,3 +101,4 @@ while True:
 
     except(KeyboardInterrupt,EOFError,TimeoutError,RuntimeError,SystemError,SystemExit):
         break
+"""
